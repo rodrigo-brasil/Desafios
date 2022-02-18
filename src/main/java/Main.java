5e34fc919b58c0eba0dcdf302fcc38dcc6262e79
Rodrigo S Brasil <maic_rodrigo@hotmail.com>
2022-02-18 11:44:16 -0300

    Add main

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import view.App;
+
+
+/**
+ * @author  Rodrigo Sousa Brasil
+ * Desafio do processor seletivo Capgemini 2022
+ */
+public class Main {
+    public static void main(String[] args) {
+        new App();
+    }
+}
